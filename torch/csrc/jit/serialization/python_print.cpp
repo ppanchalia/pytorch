3a044ffda71db8fe58e5edba0cd1c569a3707789
Aleksei Nikiforov <aleksei.nikiforov@linux.ibm.com>
2024-09-26 12:11:45 +0200

    Fix new gcc warnings: torch/csrc/jit/serialization/python_print.cpp
    /var/lib/jenkins/workspace/torch/csrc/jit/serialization/python_print.cpp: In member function ‘void torch::jit::PythonPrintImpl::printRHS(TaggedStringStream&, torch::jit::Node*)’:
    /var/lib/jenkins/workspace/torch/csrc/jit/serialization/python_print.cpp:1143:21: error: possibly dangling reference to a temporary [-Werror=dangling-reference]
     1143 |         const auto& self = node->inputs().at(0);
          |                     ^~~~
    /var/lib/jenkins/workspace/torch/csrc/jit/serialization/python_print.cpp:1143:45: note: the temporary was destroyed at the end of the full expression ‘node->torch::jit::Node::inputs().c10::ArrayRef<torch::jit::Value*>::at(0)’
     1143 |         const auto& self = node->inputs().at(0);
          |                            ~~~~~~~~~~~~~~~~~^~~

--- torch/csrc/jit/serialization/python_print.cpp
+++ torch/csrc/jit/serialization/python_print.cpp
@@ -1140,7 +1140,7 @@ struct PythonPrintImpl {
         stmt << ")";
       } break;
       case prim::CallMethod: {
-        const auto& self = node->inputs().at(0);
+        const auto self = node->inputs().at(0);
         const auto& methodName = node->s(attr::name);
         stmt << "(" << useOf(self) << ")"
              << "." << methodName << "(";
